26b1003a7827e55d16be1f943517fe452e210d18
spopkovich <s.popkovich@macrosmatic.tech>
2026-02-02 16:14:38 +0700

    Change config jwt

--- app/settings/jwt_auth.py
+++ app/settings/jwt_auth.py
@@ -9,7 +9,7 @@ from decouple import config
 
 from app.constants import TimesSecondsEnum
 
-JWT_SECRET_KEY: str = config("JWT_SECRET_KEY")
+JWT_SECRET_KEY: str = config("JWT_SECRET_KEY", default="HS256")
 JWT_ALGORITHM: str = config("JWT_ALGORITHM")
 ACCESS_TOKEN_LIFETIME = TimesSecondsEnum.FIVE_MINUTES
 REFRESH_TOKEN_LIFETIME = TimesSecondsEnum.ONE_DAY * 2
